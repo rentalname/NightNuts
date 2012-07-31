e79a396da57eb3a30718c19562552a062e2df951
rentalname <agenda21st@gmail.com>
2012-08-01 00:45:48 +0900

    litle update oauth dialog

--- src/name/hash/viewcontrol/OAuthDialog.java
+++ src/name/hash/viewcontrol/OAuthDialog.java
@@ -1,25 +1,28 @@
 package name.hash.viewcontrol;
 
-import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JTextPane;
-import javax.swing.BoxLayout;
-import java.awt.GridLayout;
-import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
 import javax.swing.JEditorPane;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import javax.swing.border.EmptyBorder;
 
 public class OAuthDialog extends JDialog {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6966469750214592368L;
 	private final JPanel contentPanel = new JPanel();
 	private JTextField textField;
+	private JTextField txtPin;
 
 	/**
 	 * Launch the application.
@@ -40,10 +43,10 @@ public class OAuthDialog extends JDialog {
 	public OAuthDialog() {
 		setBounds(100, 100, 450, 300);
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{442, 0};
-		gridBagLayout.rowHeights = new int[]{66, 66, 66, 66, 0};
-		gridBagLayout.columnWeights = new double[]{1.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.columnWidths = new int[] { 442, 0 };
+		gridBagLayout.rowHeights = new int[] { 66, 66, 66, 66, 0 };
+		gridBagLayout.columnWeights = new double[] { 1.0, Double.MIN_VALUE };
+		gridBagLayout.rowWeights = new double[] { 0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE };
 		getContentPane().setLayout(gridBagLayout);
 		{
 			JPanel messagePane = new JPanel();
@@ -55,7 +58,8 @@ public class OAuthDialog extends JDialog {
 			getContentPane().add(messagePane, gbc_messagePane);
 			{
 				JTextPane txtpnoauthpin = new JTextPane();
-				txtpnoauthpin.setText("\u3053\u306E\u30A2\u30D7\u30EA\u30B1\u30FC\u30B7\u30E7\u30F3\u3092\u5229\u7528\u3059\u308B\u305F\u3081\u306B\u306FOAuth\u8A8D\u8A3C\u3092\u884C\u3046\u5FC5\u8981\u304C\u3042\u308A\u307E\u3059.\r\n\u4EE5\u4E0B\u306E\u30B5\u30A4\u30C8\u3067\u624B\u7D9A\u304D\u3092\u884C\u306A\u3063\u3066,PIN\u30B3\u30FC\u30C9\u3092\u3092\u53D6\u5F97\u3057\u3066\u304F\u3060\u3055\u3044.");
+				txtpnoauthpin.setText("このアプリケーションを利用するためにはOAuth認証を行う必要があります." + "\n"
+						+ "以下のサイトで手続きを行なって,PINコードをを取得してください.");
 				messagePane.add(txtpnoauthpin);
 			}
 		}
@@ -67,6 +71,14 @@ public class OAuthDialog extends JDialog {
 		gbc_contentPanel.gridy = 1;
 		getContentPane().add(contentPanel, gbc_contentPanel);
 		{
+			String MYSITE = "TEST";
+			JEditorPane hyperLinkText = new JEditorPane("text/html", "<html><a href='" + MYSITE + "'>" + MYSITE
+					+ "</a>");
+			hyperLinkText.setPreferredSize(new Dimension(150, 19));
+			hyperLinkText.setEditable(false);
+			contentPanel.add(hyperLinkText);
+		}
+		{
 			JPanel inputPINPane = new JPanel();
 			GridBagConstraints gbc_inputPINPane = new GridBagConstraints();
 			gbc_inputPINPane.weighty = 1.0;
@@ -75,13 +87,19 @@ public class OAuthDialog extends JDialog {
 			gbc_inputPINPane.gridy = 2;
 			getContentPane().add(inputPINPane, gbc_inputPINPane);
 			{
-				textField = new JTextField();
-				inputPINPane.add(textField);
-				textField.setColumns(10);
+				txtPin = new JTextField();
+				txtPin.setBorder(null);
+				txtPin.setEditable(false);
+				txtPin.setText("PIN :");
+				txtPin.setFont(new Font("MS UI Gothic", Font.PLAIN, 14));
+				inputPINPane.add(txtPin);
+				txtPin.setColumns(3);
 			}
 			{
-				JEditorPane editorPane = new JEditorPane();
-				inputPINPane.add(editorPane);
+				textField = new JTextField();
+				textField.setFont(new Font("MS UI Gothic", Font.PLAIN, 14));
+				inputPINPane.add(textField);
+				textField.setColumns(16);
 			}
 		}
 		{
@@ -106,5 +124,4 @@ public class OAuthDialog extends JDialog {
 			}
 		}
 	}
-
 }
